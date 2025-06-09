b501a3a53fefa4abc8b3b12d7a299d63d6e079d4
Evgeny <jacksonou3@gmail.com>
2025-06-09 11:12:17 +0300

    The BookingRepository class is implemented, which is an abstract base class that defines a contract (interface) for working with the booking repository. It does not contain a concrete implementation, but only describes what methods should be implemented in any classes inherited from it.

--- src/api/core/booking.py
+++ src/api/core/booking.py
@@ -1,4 +1,7 @@
 from datetime import date
+from abc import ABC, abstractmethod
+from booking import Booking
+from typing import List, Optional
 
 class Booking:
     def __init__(
@@ -12,3 +15,28 @@ class Booking:
         self.room_id = room_id
         self.check_in = check_in
         self.check_out = check_out
+
+    def __repr__(self):
+        return f'Booking {self.room_id}, check in date - {self.check_in}, check out date {self.check_out}'
+    
+class BookingRepository(ABC):
+
+    @abstractmethod
+    def add_booking(self, booking: Booking) -> None:
+        pass
+
+    @abstractmethod
+    def get_booking(self, booking_id: int) -> Optional[Booking]:
+        pass
+
+    @abstractmethod
+    def change_booking(self, booking: Booking) -> None:
+        pass
+
+    @abstractmethod
+    def get_booking_by_id(self, room_id: int) -> List[Booking]:
+        pass
+
+    @abstractmethod
+    def delete_booking(self, booking_id: int) -> None:
+        pass
