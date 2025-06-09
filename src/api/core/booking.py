2cce6f2b18248734a3292f2362d8e629c4a924e3
Evgeny <jacksonou3@gmail.com>
2025-06-09 15:24:59 +0300

    fixing

--- src/api/core/booking.py
+++ src/api/core/booking.py
@@ -83,38 +83,13 @@ class BookingManager:
         if check_out <= check_in:
             raise ValueError
         
-        bookings = self.repository.get_booking_by_room(room_id=room_id)
+        bookings = self.repository.get_booking_by_room(room_id)
         for booking in bookings:
             if exclude_booking_id and booking.id == exclude_booking_id:
                 continue
             if not (check_out <= booking.check_in or check_in >= booking.check_out):
                 return False
         return True
-
-
-# ==== Проверка ====
-repo = InMemoryRepository()
-
-# Допустим, у нас есть номер "room1"
-room = Room('freg' ,'lol', 4, 2500, 'b1')
-hotel = Hotel("hotel1", "Test Hotel", [room])
-manager = BookingManager(repo, [hotel])
-
-# Добавим бронирование с 10 по 15 июня
-existing_booking = Booking(id="b1", room_id="room1", check_in=date(2025, 6, 10), check_out=date(2025, 6, 15), guest_count=1)
-repo.create_booking(existing_booking)
-
-# Тест 1: проверим пересечение (например, с 12 по 17 июня) → False
-print(manager.is_available("room1", date(2025, 6, 12), date(2025, 6, 17), 1))  # ❌ False
-
-# Тест 2: проверим свободный диапазон (например, с 5 по 9 июня) → True
-print(manager.is_available("room1", date(2025, 6, 5), date(2025, 6, 9)))  # ✅ True
-
-# Тест 3: заезд в день выезда (15 июня) → True
-print(manager.is_available("room1", date(2025, 6, 15), date(2025, 6, 18)))  # ✅ True
-
-# Тест 4: заезд = выезд (некорректно) → False
-print(manager.is_available("room1", date(2025, 6, 20), date(2025, 6, 20)))  # ❌ False
         
 
         
