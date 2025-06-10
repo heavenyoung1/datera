97ff1a7b74c1333619ff80eb13860a84dd60c361
Evgeny <jacksonou3@gmail.com>
2025-06-10 12:49:25 +0300

    Changing class Room

--- src/api/core/room.py
+++ src/api/core/room.py
@@ -3,13 +3,8 @@ from dataclasses import dataclass
 @dataclass
 class Room:
     id: str
-    name: str
-    capacity: int
+    type: tuple
+    max_guests: int
+    floor: int
     price: int
     hotel_id: str
-
-    def __repr__(self):
-        return f'Booking {self.name}, capacity - {self.capacity}, price per night {self.price}'
-    
-    def __eq__(self, value):
-        pass #Реализовать функцию сравнения
