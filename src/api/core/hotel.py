b2dd827cb9e44c67ac2f0f2adf257a3fc0518099
Evgeny <jacksonou3@gmail.com>
2025-06-10 13:13:11 +0300

    For each class added docstring

--- src/api/core/hotel.py
+++ src/api/core/hotel.py
@@ -5,6 +5,22 @@ from src.api.core.room import Room
 
 @dataclass
 class Hotel:
+    """ Модель отеля, содержащая информацию о нём и его номерах.
+
+        :ivar id: Уникальный идентификатор отеля.
+        :vartype id: str
+        :ivar name: Название отеля.
+        :vartype name: str
+        :ivar city: Город, где расположен отель.
+        :vartype city: str
+        :ivar address: Адрес отеля.
+        :vartype address: str
+        :ivar metro_stations: Список ближайших станций метро.
+        :vartype metro_stations: List[str]
+        :ivar rooms: Список номеров в отеле.
+        :vartype rooms: List[Room]
+        
+    """
     id: str
     name: str
     city: str
