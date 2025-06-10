eb8cba45aba8ffe63b5fd73a91e8ccd681a6a5b3
Evgeny <jacksonou3@gmail.com>
2025-06-10 12:47:48 +0300

    Cleaning and cnanging class Hotel

--- src/api/core/hotel.py
+++ src/api/core/hotel.py
@@ -1,10 +1,17 @@
 from dataclasses import dataclass
+from typing import List
+
+from src.api.core.room import Room
+
 
 @dataclass
 class Hotel:
     id: str
     name: str
-    rooms: int
+    city: str
+    address: str
+    metro_stations: List[str]
+    rooms: List[Room]
 
     def __repr__(self):
         return f'Hotel with ID {self.id}, {self.id}, name - {self.name}, rooms {self.rooms}'
