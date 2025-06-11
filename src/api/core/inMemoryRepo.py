add0a3ac7397cf243dac031ec487027f4927e5cb
Evgeny <jacksonou3@gmail.com>
2025-06-11 15:03:50 +0300

    little fix

--- src/api/core/inMemoryRepo.py
+++ src/api/core/inMemoryRepo.py
@@ -22,12 +22,11 @@ class InMemoryRepository(BookingRepository):
     def get_booked_by_id(self, booking_id: str) -> Optional[Booking]:
         return self.bookings.get(booking_id)
     
-    def get_booked_room(self, room_id: str, date: pendulum.DateTime):
+    def get_booked_room(self, room_id: str, date: pendulum.DateTime) -> Optional[Booking]:
         for booking in self.bookings.values():
             if (room_id == booking.room_id):
                  return booking
         return None
-        
 
     def update_book(self, booking_id: str, check_in: pendulum.DateTime, check_out: pendulum.DateTime) -> Booking:
         booking = self.get_booked_by_id(booking_id)
@@ -39,7 +38,6 @@ class InMemoryRepository(BookingRepository):
         return booking
 
     def delete_book(self, booking_id: str) -> None:
-        if not any(booking_id == booking.id for booking in self.bookings):
-            raise ValueError(f'Booking {booking_id} not found')
-        self.bookings = [booking for booking in self.bookings if booking_id != booking.id]
-        
+        if booking_id not in self.bookings:
+            return None
+        return self.bookings[booking_id]
